5eb61d25e4d3ce162df6643c2684449c4b54d41a
dylan <wuxiewang@gmail.com>
2019-11-11 01:47:17 +0800

    Fixed: abc

--- src/pages/components/HelloWorld.vue
+++ src/pages/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>computed name: {{ computedName }}</p>
-    <el-button @click="() => ''">click</el-button>
+    <el-button @click="() => ''" style="color: red;">click</el-button>
   </div>
 </template>
 
