c56dede09ae6292777e1e563214b06f1935284b4
dylan-7 <wuxiewang@gmail.com>
2019-12-03 16:21:04 +0800

    Added: 123

--- src/store/modules/permission.ts
+++ src/store/modules/permission.ts
@@ -1,31 +1,25 @@
-import {
-  VuexModule,
-  Module,
-  Mutation,
-  Action,
-  getModule
-} from 'vuex-module-decorators';
-import { RouteConfig } from 'vue-router';
+import { VuexModule, Module, Mutation, Action, getModule } from 'vuex-module-decorators'
+import { RouteConfig } from 'vue-router'
 import { constantRoutes, asyncRoutes } from '@/routes';
-import store from '@/store';
+import store from '@/store'
 
 const routeFillter = (route: RouteConfig[]) => {
   return route;
-};
+}
 
 @Module({ dynamic: true, store, name: 'permission' })
 class Permission extends VuexModule implements PermissionState {
-  public dynamicRoutes: RouteConfig[] = [];
+  public dynamicRoutes: RouteConfig[] = []
 
   @Mutation
   private SET_ROUTES(routes: RouteConfig[]) {
-    this.dynamicRoutes = constantRoutes.concat(routes);
+    this.dynamicRoutes = constantRoutes.concat(routes)
   }
 
   @Action
   public GenerateRoutes() {
     const accessedRoutes = routeFillter(asyncRoutes);
-    this.SET_ROUTES(accessedRoutes);
+    this.SET_ROUTES(accessedRoutes)
   }
 }
 
